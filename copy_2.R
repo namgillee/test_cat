1b3ee6e1e8c1f7327bf373f953724306a6d0b1c5
Namgil Lee <namgil.lee@kangwon.ac.kr>
2019-07-12 15:00:53 +0900

    new commit 2

--- copy_2.R
+++ copy_2.R
@@ -4,4 +4,4 @@ with(cats, plot(Bwt, Hwt, col = Sex, pch = c(1,3)[Sex]))
 legend("topleft", legend = levels(cats$Sex), col = 1:2, pch = c(1,3))
 
 #This is a test code
-#for testing pull and merge
+#for testing merge
